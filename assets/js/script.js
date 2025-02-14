159de2ced4dc911b01ce31d1a35a6b750e4c9423
ErnestKyei <kyeiernest86@gmail.com>
2025-02-14 06:03:22 +0000

    Initial commit - InfotechHub project

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+'use strict';
+
+/**
+ * Add event on elements
+ */
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+};
+
+/**
+ * Toggle Navbar
+ */
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+};
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+};
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+/**
+ * Header Active on Scroll
+ */
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+/**
+ * Dynamic Date in Footer
+ */
+/**
+ * Dynamic Date in Footer
+ */
+function formatDate(date) {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return date.toLocaleDateString('en-US', options);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const footerModifiedDateElement = document.querySelector('.footer-modified-date');
+  if (footerModifiedDateElement) {
+    const lastModified = new Date(document.lastModified);
+    footerModifiedDateElement.textContent = formatDate(lastModified);
+  }
+});
